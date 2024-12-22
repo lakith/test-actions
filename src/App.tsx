6ebba73e6b360cc676a6cbdcbf0d0852c296f33f
lakith <lakith1995@gmail.com>
2024-12-22 21:36:50 +0530

    change base

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import {lazy, Suspense} from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
-
-import './App.css'
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Fixed import from 'react-router-dom'
+import './App.css';
 
 function App() {
+  const CityOfLanding = lazy(() =>
+    import('./pages/cityOfferDetailLanding/CityOfferDetailLanding.tsx')
+  );
+  const CityOfferLanding = lazy(() =>
+    import('./pages/cityOfferLanding/CityOfferLanding.tsx')
+  );
+  const Home = lazy(() => import('./pages/home/Home.tsx'));
 
- const CityOfLanding = lazy(() => import('./pages/cityOfferDetailLanding/CityOfferDetailLanding.tsx'));
- const CityOfferLanding = lazy(() => import('./pages/cityOfferLanding/CityOfferLanding.tsx'));
- const Home = lazy(() => import('./pages/home/Home.tsx'));
-
- return (
+  return (
     <>
-        <Router>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                  <Route path="/" element={<CityOfferLanding />} />
-                  <Route path="/landing" element={<Home />} />
-                  <Route path="/landing/:id" element={<CityOfLanding />} />
-                </Routes>
-            </Suspense>
-        </Router>
+      {/* Add basename property to the Router */}
+      <Router basename="/test-actions">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<CityOfferLanding />} />
+            <Route path="/landing" element={<Home />} />
+            <Route path="/landing/:id" element={<CityOfLanding />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
